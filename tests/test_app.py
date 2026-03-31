060539bc9bbfdda31dee08d4323057dd59506733
Eskan0r1 <philippe.kuang13@gmail.com>
2026-03-31 14:36:19 +0200

    Fix tests en mockant Redis

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,12 +6,12 @@ class AppTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    @patch("app.redis.Redis")  # on remplace Redis par un mock
+    @patch("app.redis.Redis")  # Mock Redis pour le test
     def test_home(self, mock_redis):
-        # On configure le mock
+        # Configure le mock pour retourner des valeurs réalistes
         mock_instance = MagicMock()
         mock_instance.exists.return_value = True
-        mock_instance.get.return_value = b"42"
+        mock_instance.get.return_value = b"1"
         mock_redis.return_value = mock_instance
 
         response = self.app.get('/')
