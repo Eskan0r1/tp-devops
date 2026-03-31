e4d7c8c9783fb103a84e3a17667d913c133f16fa
Eskan0r1 <philippe.kuang13@gmail.com>
2026-03-31 14:22:14 +0200

    Corrige tests avec mock Redis pour CI

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,19 @@
 import unittest
+from unittest.mock import patch, MagicMock
 from app import app
 
 class AppTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def test_home(self):
+    @patch("app.redis.Redis")  # on remplace Redis par un mock
+    def test_home(self, mock_redis):
+        # On configure le mock
+        mock_instance = MagicMock()
+        mock_instance.exists.return_value = True
+        mock_instance.get.return_value = b"42"
+        mock_redis.return_value = mock_instance
+
         response = self.app.get('/')
         self.assertIn("Visites :", response.data.decode())
 
