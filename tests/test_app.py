99d75a791d14732eeb472ffdd2f10e5317e854ca
Eskan0r1 <philippe.kuang13@gmail.com>
2026-03-31 16:02:02 +0200

    TP6 : version bug v1.1

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,21 @@
 import unittest
+from unittest.mock import patch, MagicMock
 from app import app
 
 class AppTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def test_home(self):
+    @patch("app.redis.Redis")  # Mock Redis
+    def test_home(self, mock_redis_class):
+        # Crée un mock de Redis
+        mock_redis_instance = MagicMock()
+        mock_redis_instance.exists.return_value = True
+        mock_redis_instance.get.return_value = b"1"
+        mock_redis_class.return_value = mock_redis_instance
+
         response = self.app.get('/')
-        self.assertIn("Visites :", response.data.decode())
+        self.assertIn("Visites :", response.data.decode())  # Vérifie que "Visites :" est dans la réponse
 
 if __name__ == "__main__":
     unittest.main()
