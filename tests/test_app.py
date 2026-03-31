f83586f5b056fc4df1e21bc4964a53f58e0160db
Eskan0r1 <philippe.kuang13@gmail.com>
2026-03-31 15:25:23 +0200

    TP5 : correction test unitaire pour Hello DevOps

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,21 +1,13 @@
 import unittest
-from unittest.mock import patch, MagicMock
 from app import app
 
 class AppTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    @patch("app.redis.Redis")  # Mock Redis pour le test
-    def test_home(self, mock_redis):
-        # Configure le mock pour retourner des valeurs réalistes
-        mock_instance = MagicMock()
-        mock_instance.exists.return_value = True
-        mock_instance.get.return_value = b"1"
-        mock_redis.return_value = mock_instance
-
+    def test_hello(self):
         response = self.app.get('/')
-        self.assertIn("Visites :", response.data.decode())
+        self.assertEqual(response.data.decode(), "Hello DevOps")
 
 if __name__ == "__main__":
     unittest.main()
